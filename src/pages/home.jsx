e913ebea598a2723455427f0015d534dac9a243f
jornadadainclusao <jornadadainclusao@gmail.com>
2025-03-20 12:06:05 -0300

    1.0.20 ajuste no CSS da home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
 import Header from "../components/home/Header.jsx";
 import Carousel from "../components/home/Carousel.jsx"
 import Cards from "../components/home/Cards.jsx"
-import Contact from "../components/home/Contact.jsx";
-import About from "../components/home/About.jsx";
+import Contact from '../components/home/Contact.jsx';
+import About from '../components/home/About.jsx';
 import Footer from "../components/home/Footer.jsx"
 
 import styles from '../assets/styles/home.module.css';
 
-// BUG: Carousel broken after setting defaults in App.css
+
 const Home = () => (
   <div className={styles.home}>
     <Header />
-    <main>
-      <Carousel />
-      <div className="heading-section">
-        <h1 className="heading">Confira os jogos da nossa plataforma</h1>
-      </div>
-      <Cards />
-      <About />
-      <Contact />
-    </main>
+    <Carousel />
+    <h1>Confira os jogos da nossa plataforma</h1>
+    <Cards />
+    <About />
+    <Contact />
     <Footer />
   </div>
 );
 
 export default Home;
+
