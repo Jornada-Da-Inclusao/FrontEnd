70da355ebdb3fb41e15d603e0b999b973b33be46
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2025-04-03 12:50:25 -0300

    partial avatar charger

--- src/components/perfil/cadastrarDependente/CadastrarDep.jsx
+++ src/components/perfil/cadastrarDependente/CadastrarDep.jsx
@@ -19,10 +19,40 @@ const CadastroForm = () => {
         alert("Cadastro realizado com sucesso!");
     };
 
+
+    // view this in home: https://www.svgrepo.com/svg/420338/avatar-person-pilot
+    const icons = [
+        {
+            0: "https://www.svgrepo.com/show/420338/avatar-person-pilot.svg",
+            1: "https://www.svgrepo.com/show/420314/builder-helmet-worker.svg",
+            2: "https://www.svgrepo.com/show/420315/avatar-cloud-crying.svg",
+            3: "https://www.svgrepo.com/show/420316/indian-man-sikh.svg",
+            4: "https://www.svgrepo.com/show/420329/anime-away-face.svg",
+            5: "https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg",
+            6: "https://www.svgrepo.com/show/420333/afro-avatar-male.svg",
+            7: "https://www.svgrepo.com/show/420347/avatar-einstein-professor.svg"
+        }
+    ]
+
+    console.log(icons)
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <h2>Cadastrar dependentes</h2>
+
+                <h3>escolha um avatar:</h3>
+                <div className={styles.figures}>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                    <button type="button"><img src={icons[0]} /></button>
+                </div>
+
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="nome">Nome da Criança:</label>
                     <input type="text" />
@@ -38,7 +68,7 @@ const CadastroForm = () => {
                     <button type="submit">Cadastrar</button>
                 </form>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/perfil/editar/editarCadUser.jsx
+++ src/components/perfil/editar/editarCadUser.jsx
@@ -5,12 +5,14 @@ const EditarUsuario = () => {
         <div className={styles.container}>
             <div className={styles.content}>
                 <form action="">
-                    <h2>Editar dados do responsavel</h2>
+                    <h2>Editar dados do responsável</h2>
                     <label htmlFor="nome">Alterar nome:</label>
                     <input type="text" />
                     <label htmlFor="email">Alterar E-mail:</label>
                     <input type="text" />
                     <label htmlFor="senha">Alterar senha:</label>
+                    <input type="password" />
+                    <label htmlFor="senha">Digite a nova senha novamente:</label>
                     <input type="password" />
                     <button>Alterar dados</button>
                 </form>
