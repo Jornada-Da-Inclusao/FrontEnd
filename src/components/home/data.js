feb980e69914f1386ab86333b751a67c332475b5
renatowljacob <renato.jacob@fatec.sp.gov.br>
2026-05-18 06:31:01 -0300

    Revert "Deduplicate text data"
    This reverts commit b8e7759f12a4ca99d04aee0e78cdd8d2e29f2610.

--- src/components/home/data.js
+++ src/components/home/data.js
@@ -2,7 +2,6 @@ import imgCores from "../../assets/images/cores.jpg"
 import imgMemoria from "../../assets/images/memoria.png"
 import imgNumeros from "../../assets/images/numeros.png"
 import imgVogais from "../../assets/images/vogais.jpg"
-import { gameDescriptions } from "../../content/gameDescriptions.js"
 
 export const cardsData = [
 	{
@@ -10,7 +9,7 @@ export const cardsData = [
 		name: "cores",
 		alt: "Jogo das Cores",
 		title: "Jogo das Cores",
-		desc: gameDescriptions.cores,
+		desc: "O Jogo das Cores ensina as crianças a identificar e nomear cores através de atividades lúdicas. Além de estimular a observação, ajuda a expandir o vocabulário e a criatividade.",
 		descModal: "No Jogo das Cores, o objetivo é arrastar cada uma das cores para o container do animal correspondente.",
 		img: imgCores,
 		link: "/jogo-cores",
@@ -21,7 +20,7 @@ export const cardsData = [
 		name: "memoria",
 		alt: "Jogo da Memória",
 		title: "Jogo da Memória",
-		desc: gameDescriptions.memoria,
+		desc: "O Jogo da Memória ajuda as crianças a melhorarem a concentração e a memória visual. Ao encontrar pares de cartas relacionadas a conceitos educativos, os alunos reforçam o aprendizado de forma divertida.",
 		descModal: "No Jogo da Memória, as cartas começarão viradas de cabeça para baixo, esperando um clique para serem reveladas. Caso duas cartas iguais sejam selecionadas consecutivamente, elas serão tidas como reveladas, do contrário, elas voltarão a ficar ocultas. Seu objetivo é revelar todas as cartas.",
 		img: imgMemoria,
 		link: "/jogo-memoria",
@@ -32,7 +31,7 @@ export const cardsData = [
 		name: "numeros",
 		alt: "Jogo dos Números",
 		title: "Jogo dos Números",
-		desc: gameDescriptions.numeros,
+		desc: "Neste jogo, as crianças praticam a ordenação de números em sequência crescente ou decrescente. Essa atividade desenvolve o raciocínio lógico e familiariza os alunos com a sequência numérica.",
 		descModal: "No Jogo dos Números, o objetivo é ordernar todos os números em sequência numérica ao arrastá-los para o container.",
 		img: imgNumeros,
 		link: "/jogo-numeros",
@@ -43,7 +42,7 @@ export const cardsData = [
 		name: "vogais",
 		alt: "Jogo das Vogais",
 		title: "Jogo das Vogais",
-		desc: gameDescriptions.vogais,
+		desc: "Neste jogo, as crianças aprendem sobre vogais de forma interativa. Através de atividades de reconhecimento e combinação, desenvolvem habilidades de leitura e aumentam o interesse pela linguagem.",
 		descModal: "No Jogo das Vogais, o objetivo é arrastar, dentre o alfabeto inteiro, apenas as letras vogais para o container.",
 		img: imgVogais,
 		link: "/jogo-vogais",
--- src/services/gcs-audio.service.js
+++ src/services/gcs-audio.service.js
@@ -2,8 +2,6 @@
  * Service for building GCS URLs and managing audio content mapping
  */
 
-import { gameDescriptions } from '../content/gameDescriptions.js';
-
 const GCS_BUCKET_URL = 'https://storage.googleapis.com/integra-kids-voices';
 
 /**
@@ -27,19 +25,19 @@ export const audioContentMap = {
     audioFile: 'explicacoes/introducao.mp3',
   },
   explicacao_cores: {
-    text: gameDescriptions.cores,
+    text: 'O Jogo das Cores ensina as crianças a identificar e nomear cores através de atividades lúdicas. Além de estimular a observação, ajuda a expandir o vocabulário e a criatividade.',
     audioFile: 'explicacoes/cores/description.mp3',
   },
   explicacao_memoria: {
-    text: gameDescriptions.memoria,
+    text: 'O Jogo da Memória ajuda as crianças a melhorarem a concentração e a memória visual. Ao encontrar pares de cartas relacionadas a conceitos educativos, os alunos reforçam o aprendizado de forma divertida.',
     audioFile: 'explicacoes/memoria/description.mp3',
   },
   explicacao_numeros: {
-    text: gameDescriptions.numeros,
+    text: 'Neste jogo, as crianças praticam a ordenação de números em sequência crescente ou decrescente. Essa atividade desenvolve o raciocínio lógico e familiariza os alunos com a sequência numérica.',
     audioFile: 'explicacoes/numeros/description.mp3',
   },
   explicacao_vogais: {
-    text: gameDescriptions.vogais,
+    text: 'Neste jogo, as crianças aprendem sobre vogais de forma interativa. Através de atividades de reconhecimento e combinação, desenvolvem habilidades de leitura e aumentam o interesse pela linguagem.',
     audioFile: 'explicacoes/vogais/description.mp3',
   },
   // Nossa Página section paragraphs
